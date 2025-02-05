67895897daae1831623e93d7a32d7d130a3127de
GoodWell-cpu <wellingtonhildo@hotmail.com>
2025-02-05 21:22:17 +0000

    Inserido as cartas supertrunfo de sao paulo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,65 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
+int main(){
+    char nome[20] = "Sao Paulo";
+    char Codigo[20] = "A1";
+    float População = 23.1;  
+    float Área = 1.523 ;
+    float PIB = 828.98 ; 
+    int PontosTuristicos = 36; 
 
-int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    printf("Nome: %s\n ", nome);
+    printf("Codigo: %s\n ", Codigo);
+    printf("População: %f\n ", População);
+    printf("Área: %f\n ", Área);
+    printf("PIB: %f\n ", PIB);
+    printf("PontosTuristicos: %d\n ", PontosTuristicos);
 
-    return 0;
-}
+    char  Nome2[20] = "araras";
+    char  codigo2[20] = "A2";
+    float população2 = 128.895;  
+    float Área2 = 644.831 ;
+    float PIB2 = 6.942 ; 
+    int  PontosTuristicos2 = 15; 
+
+    printf("Nome: %s\n ", Nome2);
+    printf("Codigo: %s\n ", codigo2);
+    printf("População: %f\n ", população2);
+    printf("Área: %f\n ", Área2);
+    printf("PIB: %f\n ", PIB2);
+    printf("PontosTuristicos: %d\n ", PontosTuristicos2);
+
+    char nome3[20] = "Mogi Guaçu";
+    char Codigo3[20] = "A3";
+    float População3 = 153.658;  
+    float Área3 = 812.75 ;
+    float PIB3 = 6.865 ; 
+    int PontosTuristicos3 = 12 ; 
+
+    printf("Nome: %s\n ", nome3);
+    printf("Codigo: %s\n ", Codigo3);
+    printf("População: %f\n ", População3);
+    printf("Área: %f\n ", Área3);
+    printf("PIB: %f\n ", PIB3);
+    printf("PontosTuristicos: %d\n ", PontosTuristicos3);
+
+    char nome4[20] = "Araraquara";
+    char Codigo4[20] = "A4";
+    float População4 = 252.318;  
+    float Área4 = 1.004 ;
+    float PIB4 = 12.102 ; 
+    int PontosTuristicos4 = 16; 
+
+    printf("Nome: %s\n ", nome4);
+    printf("Codigo: %s\n ", Codigo4);
+    printf("População: %f\n ", População4);
+    printf("Área: %f\n ", Área4);
+    printf("PIB: %f\n ", PIB4);
+    printf("PontosTuristicos: %d\n ", PontosTuristicos4);
+
+   
+
+
+   
+
+} 
