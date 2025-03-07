c2e13e73007e4e4fb644722e29f7dd7440893334
GoodWell-cpu <wellingtonhildo@hotmail.com>
2025-03-07 14:59:04 +0000

    incluido 2 cartas SP e RJ

---
+++ CartasSuperTrunfo .c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int main(){
+    int numerodacarta = 1;
+    char estado[20] = "A";
+    char codigo[20] = "A01";
+    char nomedacidade[20] = "Sao Paulo";
+    int populacao = 11450000;  
+    float area = 248.21 ;
+    float pib = 828.98 ; 
+    int pontosturisticos = 36; 
+
+    printf("Carta: %d\n ", numerodacarta);
+    printf("Estado: %s\n ", estado);
+    printf("Nome da Cidade: %s\n ", nomedacidade);
+    printf("Codigo: %s\n ", codigo);
+    printf("Populacao: %d\n ", populacao);
+    printf("Area: %f Km²\n ", area);
+    printf("PIB: %f Bilhoes de Reais\n ", pib);
+    printf("Pontos Turisticos: %d \n ", pontosturisticos);
+
+    int numerodacarta2 = 2;
+    char estado2[20] = "B";
+    char Codigo2[20] = "B02";
+    char nomedacidade2[20] = "Rio de Janeiro";
+    int populacao2 = 6211000;  
+    float area2 = 43.69 ;
+    float PIB2 = 949.30 ; 
+    int pontosturisticos2 = 36; 
+
+    printf("Carta: %d\n ", numerodacarta2);
+    printf("Estado: %s\n ", estado2);
+    printf("Nome da Cidade: %s\n ", nomedacidade2);
+    printf("Codigo: %s\n ", Codigo2);
+    printf("Populacao: %d\n ", populacao2);
+    printf("Area: %f Km²\n ", area2);
+    printf("PIB: %f Bilhoes de Reais\n ", PIB2);
+    printf("Pontos Turisticos: %d\n ", pontosturisticos2);
+
+
+
+
+return 0;
+
+} 
