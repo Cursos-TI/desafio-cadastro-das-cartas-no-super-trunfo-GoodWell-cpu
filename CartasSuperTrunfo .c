a55442d7bb643af2b4d1726985128279d3e29ac5
GoodWell-cpu <wellingtonhildo@hotmail.com>
2025-03-08 15:16:07 +0000

    adicionado if e else comparando 2 cartas

--- CartasSuperTrunfo .c
+++ CartasSuperTrunfo .c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 int main(){
+    // variaveis da primeira carta
     int numerodacarta = 1;
     char estado[20] = "A";
     char codigo[20] = "A01";
@@ -9,6 +10,8 @@ int main(){
     float area = 248.21 ;
     float pib = 828.98 ; 
     int pontosturisticos = 36; 
+    float densidadepopulacional = 7.528;
+    float pibpercapita = 66.827;
 
     printf("Carta: %d\n ", numerodacarta);
     printf("Estado: %s\n ", estado);
@@ -18,7 +21,10 @@ int main(){
     printf("Area: %f Km²\n ", area);
     printf("PIB: %f Bilhoes de Reais\n ", pib);
     printf("Pontos Turisticos: %d \n ", pontosturisticos);
+    printf("Densidade Populacional: %f Habitantes/Km² \n ", densidadepopulacional);
+    printf("PIB Per Capita: %f Mil Reais \n ", pibpercapita);
 
+    // variaveis da segunda carta
     int numerodacarta2 = 2;
     char estado2[20] = "B";
     char Codigo2[20] = "B02";
@@ -27,6 +33,8 @@ int main(){
     float area2 = 43.69 ;
     float PIB2 = 949.30 ; 
     int pontosturisticos2 = 36; 
+    float densidadepopulacional2 = 6.729;
+    float pibpercapita2 = 49.094;
 
     printf("Carta: %d\n ", numerodacarta2);
     printf("Estado: %s\n ", estado2);
@@ -36,10 +44,19 @@ int main(){
     printf("Area: %f Km²\n ", area2);
     printf("PIB: %f Bilhoes de Reais\n ", PIB2);
     printf("Pontos Turisticos: %d\n ", pontosturisticos2);
+    printf("Densidade Populacional: %f Habitantes/Km² \n ", densidadepopulacional2);
+    printf ("PIB Per Capita: %f Mil Reais \n ", pibpercapita2);
 
+    //condicao verdadeira
+    if (pib > PIB2) // Se o pib da cidade 1 for maior que o pib da cidade 2 a carta com maior numero vence
+    {
+        printf("Carta 1 Venceu - %s (SP):828.980.000\n", nomedacidade); 
+       
+    }
+    // condicao falsa
+    else{ 
 
+        printf("Carta 2 Venceu - %s (RJ):949.300.000\n", nomedacidade2);
+    }
 
-
-return 0;
-
-} 
+}
